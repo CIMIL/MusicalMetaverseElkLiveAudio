b4f56308be512c9c15a470c0377307008b077b27
Ovidiu Costin Andrioaia <ovidiu.andrioaia@yahoo.it>
2025-03-10 17:17:37 +0100

    Implements "looking at" logging events and adds some minor details to the scene

--- Assets/Assets/Scripts/LogEmitters/MainCameraLogEmitter.cs
+++ Assets/Assets/Scripts/LogEmitters/MainCameraLogEmitter.cs
@@ -3,23 +3,84 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using TinyJson;
+using Ubiq.Avatars;
 using Ubiq.Logging;
 using Ubiq.Messaging;
 using UnityEngine;
+using UnityEngine.Events;
+using Avatar = Ubiq.Avatars.Avatar;
 
 public class MainCameraLogEmitter : MonoBehaviour
 {
-    private LogEmitter logEmitter;
 
+    [SerializeField] private AvatarManager avatarManager;
+    [SerializeField] private string headTag = "Player Head";
+    [SerializeField] private int raycastDistance = 25;
+    
+    private LogEmitter logEmitter;
+    private bool lookingAt = false;
+    private NetworkId lookingAtNetworkId;
+    private NetworkContext context;
+    private NetworkId myNetworkSceneId;
     private void Start()
     {
         logEmitter = new InfoLogEmitter(this);
-        
-        InvokeRepeating(nameof(_logVector), 0.0f, 0.3f);
+        InvokeRepeating(nameof(LogVector), 0.0f, 0.3f);
+        context = NetworkScene.Register(this);
+        myNetworkSceneId = NetworkScene.Find(this).Id;
     }
-
-    private void _logVector()
+    
+    private void Update()
     {
-        logEmitter.Log("Camera direction and position vectors", transform.forward, transform.position );
+        if (Physics.Raycast(transform.position, transform.forward, out var hit, raycastDistance) && 
+            hit.collider.gameObject.CompareTag(headTag))
+        {
+            if (lookingAt) return;
+            
+            lookingAt = true;
+            context.SendJson(new Message()
+            {
+                sourceId = myNetworkSceneId,
+                avatarId = hit.collider.gameObject.GetComponent<Avatar>().NetworkId,
+                targetId = NetworkId.Null
+            });
+        }
+        else if (lookingAt)
+        {
+            lookingAt = false;
+            lookingAtNetworkId = NetworkId.Null;
+            logEmitter.Log("Stopped looking at", lookingAtNetworkId);
+        }
+    }
+    
+    public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
+    {
+        var m = message.FromJson<Message>();
+
+        if (m.sourceId == myNetworkSceneId)
+        {
+            logEmitter.Log("Is looking at", m.targetId);
+            lookingAtNetworkId = m.targetId;
+        } else if (m.avatarId == avatarManager.LocalAvatar.NetworkId)
+        {
+            context.SendJson(new Message()
+            {
+                sourceId = m.sourceId,
+                avatarId = m.avatarId,
+                targetId = myNetworkSceneId
+            });
+        }
+    }
+    
+    private class Message
+    {
+        public NetworkId sourceId;
+        public NetworkId avatarId;
+        public NetworkId targetId;
+    }
+    
+    private void LogVector()
+    {
+        logEmitter.Log("Camera position and direction", transform.forward, transform.position);
     }
 }
